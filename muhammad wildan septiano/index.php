fa5b5e78953c825d817199ff14afdaeda6d7d375
Wl.se <muhammad.wildan@raharja.info>
2023-12-29 13:57:54 +0700

    modif tampilan semua

--- muhammad wildan septiano/index.php
+++ muhammad wildan septiano/index.php
@@ -1,15 +1,8 @@
 <?php
-
 // Instruksi Kerja Nomor 1.
-// Variabel $kendaraan berisi data jenis kendaraan yang dipesan dalam bentuk array satu dimensi.
-$kendaraan = ["Sedan", "Minivan", "Minibus", "Sepeda Motor", "Pickup",];
-
-// Instruksi Kerja Nomor 2.
-// Mengurutkan array $kendaraan secara Ascending.
+$kendaraan = ["Sedan", "Minivan", "Minibus", "Sepeda Motor", "Pickup"];
 sort($kendaraan);
 
-// Instruksi Kerja Nomor 6.
-// Membuat fungsi untuk menghitung biaya sewa taxi
 function hitung_sewa($biaya_platform, $jarak, $biaya_per_km)
 {
 	$nilai_sewa = $biaya_platform + ($jarak * $biaya_per_km);
@@ -17,48 +10,60 @@ function hitung_sewa($biaya_platform, $jarak, $biaya_per_km)
 }
 ?>
 
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Pemesanan Taxi Online</title>
-	<!-- Instruksi Kerja Nomor 4. -->
-	<!-- Menghubungkan dengan library/berkas CSS. -->
-	<link rel="stylesheet" href="bootstrap.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<style>
+		body {
+			background-color: #f8f9fa;
+		}
+
+		.container-border {
+			border: 1px solid #ced4da;
+			border-radius: 8px;
+			padding: 20px;
+			margin-top: 20px;
+		}
+
+		img {
+			margin-left: 10px;
+		}
+
+		.btn-submit {
+			margin-top: 10px;
+		}
+	</style>
 </head>
 
 <body>
-	<div class="container border">
-		<!-- Menampilkan judul halaman -->
+	<div class="container container-border">
 		<h3>Pemesanan Taxi Online</h3>
+		<img src="logo.jpg" alt="Taxi Online Logo" width="70">
 
-		<!-- Instruksi Kerja Nomor 5. -->
-		<!-- Menampilkan logo Taxi Online -->
-		<img src="logo.jpg" alt="" width="70">
-
-
-		<!-- Form untuk memasukkan data pemesanan. -->
 		<form action="index.php" method="post" id="formPemesanan">
-			<div class="row">
-				<!-- Masukan data nama pelanggan. Tipe data text. -->
-				<div class="col-lg-2"><label for="nama">Nama Pelanggan:</label></div>
-				<div class="col-lg-2"><input type="text" id="nama" name="nama"></div>
+			<div class="form-group row">
+				<label for="nama" class="col-lg-2 col-form-label">Nama Pelanggan:</label>
+				<div class="col-lg-4">
+					<input type="text" class="form-control" id="nama" name="nama" required>
+				</div>
 			</div>
-			<div class="row">
-				<!-- Masukan data nomor HP pelanggan. Tipe data number. -->
-				<div class="col-lg-2"><label for="noHP">Nomor HP:</label></div>
-				<div class="col-lg-2"><input type="text" id="noHP" name="noHP" maxlength="16"></div>
+			<div class="form-group row">
+				<label for="noHP" class="col-lg-2 col-form-label">Nomor HP:</label>
+				<div class="col-lg-4">
+					<input type="text" class="form-control" id="noHP" name="noHP" maxlength="16" required>
+				</div>
 			</div>
-			<div class="row">
-				<!-- Masukan pilihan jenis kendaraan. -->
-				<div class="col-lg-2"><label for="kendaraan">Jenis Kendaraan:</label></div>
-				<div class="col-lg-2">
-					<select id="kendaraan" name="kendaraan">
-						<option value="kendaraan">- Jenis kendaraan -</option>
+			<div class="form-group row">
+				<label for="kendaraan" class="col-lg-2 col-form-label">Jenis Kendaraan:</label>
+				<div class="col-lg-4">
+					<select class="form-control" id="kendaraan" name="kendaraan" required>
+						<option value="" disabled selected>- Pilih jenis kendaraan -</option>
 						<?php
-						// Instruksi Kerja Nomor 3.
-						// Menampilkan dropdown pilihan jenis kendaraan berdasarkan data pada array $kendaraan menggunakan perulangan.
 						foreach ($kendaraan as $jenis) {
 							echo '<option value="' . $jenis . '">' . $jenis . '</option>';
 						}
@@ -66,24 +71,22 @@ function hitung_sewa($biaya_platform, $jarak, $biaya_per_km)
 					</select>
 				</div>
 			</div>
-
-			<div class="row">
-				<!-- Masukan data Jarak Tempuh. Tipe data number. -->
-				<div class="col-lg-2"><label for="jarak">Jarak:</label></div>
-				<div class="col-lg-2"><input type="number" id="jarak" name="jarak" maxlength="4"></div>
+			<div class="form-group row">
+				<label for="jarak" class="col-lg-2 col-form-label">Jarak (km):</label>
+				<div class="col-lg-4">
+					<input type="number" class="form-control" id="jarak" name="jarak" maxlength="4" required>
+				</div>
 			</div>
-			<div class="row">
-				<!-- Tombol Submit -->
-				<div class="col-lg-2"><button class="btn btn-primary" type="submit" form="formPemesanan" value="Pesan" name="Pesan">Pesan</button></div>
-				<div class="col-lg-2"></div>
+			<div class="form-group row">
+				<div class="col-lg-2">
+					<button class="btn btn-primary btn-submit" type="submit" form="formPemesanan" value="Pesan" name="Pesan">Pesan</button>
+				</div>
 			</div>
 		</form>
 	</div>
+
 	<?php
-
 	if (isset($_POST['Pesan'])) {
-
-		// Variabel $dataPesanan berisi data-data pemesanan dari form dalam bentuk array.
 		$dataPesanan = [
 			'nama' => $_POST['nama'],
 			'noHP' => $_POST['noHP'],
@@ -91,68 +94,63 @@ function hitung_sewa($biaya_platform, $jarak, $biaya_per_km)
 			'jarak' => $_POST['jarak']
 		];
 
-		// Instruksi Kerja Nomor 7
-		// Simpan jarak yang telah dimasukkan oleh pengguna dalam variabel $jarak_tempuh
 		$jarak_tempuh = $dataPesanan['jarak'];
 
-		// Instruksi Kerja Nomor 8 (Percabangan)
-		// Gunakan pencabangan untuk menentukan biaya platform dan biaya sewa per kilometer
-		// Simpan biaya platform dan biaya sewa per kilometer dalam variabel $biaya_platform dan $biaya_per_km
-		if ($dataPesanan['kendaraan'] == 'Sedan') {
-			$biaya_platform = 10000;
-			$biaya_per_km = 5000;
-		} elseif ($dataPesanan['kendaraan'] == 'Minivan') {
-			$biaya_platform = 12000;
-			$biaya_per_km = 6000;
-		} elseif ($dataPesanan['kendaraan'] == 'Minibus') {
-			$biaya_platform = 15000;
-			$biaya_per_km = 10000;
-		} elseif ($dataPesanan['kendaraan'] == 'Sepeda Motor') {
-			$biaya_platform = 5000;
-			$biaya_per_km = 3000;
-		} elseif ($dataPesanan['kendaraan'] == 'Pickup') {
-			$biaya_platform = 15000;
-			$biaya_per_km = 8000;
+		$biaya_platform = 0;
+		$biaya_per_km = 0;
+
+		switch ($dataPesanan['kendaraan']) {
+			case 'Sedan':
+				$biaya_platform = 10000;
+				$biaya_per_km = 5000;
+				break;
+			case 'Minivan':
+				$biaya_platform = 12000;
+				$biaya_per_km = 6000;
+				break;
+			case 'Minibus':
+				$biaya_platform = 15000;
+				$biaya_per_km = 10000;
+				break;
+			case 'Sepeda Motor':
+				$biaya_platform = 5000;
+				$biaya_per_km = 3000;
+				break;
+			case 'Pickup':
+				$biaya_platform = 15000;
+				$biaya_per_km = 8000;
+				break;
 		}
 
-		// Instruksi kerja Nomor 9
-		// Gunakan fungsi hitung_sewa untuk menghitung biaya sewa
 		$biaya_sewa = hitung_sewa($biaya_platform, $jarak_tempuh, $biaya_per_km);
 
-		// Instruksi Kerja Nomor 10.
-		// Simpan data pemesanan yang ke dalam file JSON
 		$berkas = "data.json";
 		$dataJson = json_encode($dataPesanan, JSON_PRETTY_PRINT);
 		file_put_contents($berkas, $dataJson);
 
-
-
-		// Menampilkan data pemesanan dan total biaya sewa.
-		// KODE DI BAWAH INI TIDAK PERLU DIMODIFIKASI!!!
 		echo "
-            <br/>
-            <div class='container'>
-                <div class='row'>
-                    <div class='col-lg-2'>Nama Pelanggan:</div>
-                    <div class='col-lg-2'>" . $dataPesanan['nama'] . "</div>
-                </div>
-                <div class='row'>
-                    <div class='col-lg-2'>Nomor HP:</div>
-                    <div class='col-lg-2'>" . $dataPesanan['noHP'] . "</div>
-                </div>
-                <div class='row'>
-                    <div class='col-lg-2'>Jenis Kendaraan:</div>
-                    <div class='col-lg-2'>" . $dataPesanan['kendaraan'] . "</div>
-                </div>
-                <div class='row'>
-                    <div class='col-lg-2'>Jarak(km):</div>
-                    <div class='col-lg-2'>" . $dataPesanan['jarak'] . " km</div>
-                </div>
-                <div class='row'>
-                    <div class='col-lg-2'>Total:</div>
-                    <div class='col-lg-2'>Rp" . number_format($biaya_sewa, 0, ",", ".") . ",-</div>
-                </div>
-            </div>";
+        <div class='container container-border'>
+            <div class='row'>
+                <div class='col-lg-2'><strong>Nama Pelanggan:</strong></div>
+                <div class='col-lg-4'>" . $dataPesanan['nama'] . "</div>
+            </div>
+            <div class='row'>
+                <div class='col-lg-2'><strong>Nomor HP:</strong></div>
+                <div class='col-lg-4'>" . $dataPesanan['noHP'] . "</div>
+            </div>
+            <div class='row'>
+                <div class='col-lg-2'><strong>Jenis Kendaraan:</strong></div>
+                <div class='col-lg-4'>" . $dataPesanan['kendaraan'] . "</div>
+            </div>
+            <div class='row'>
+                <div class='col-lg-2'><strong>Jarak (km):</strong></div>
+                <div class='col-lg-4'>" . $dataPesanan['jarak'] . " km</div>
+            </div>
+            <div class='row'>
+                <div class='col-lg-2'><strong>Total:</strong></div>
+                <div class='col-lg-4'>Rp" . number_format($biaya_sewa, 0, ",", ".") . ",-</div>
+            </div>
+        </div>";
 	}
 	?>
 </body>
